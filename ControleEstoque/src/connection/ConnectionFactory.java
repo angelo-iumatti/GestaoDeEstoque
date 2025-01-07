9c121900e8074c1a38b368ab1f2dd77cd23b34ce
Freitas <angelo.iumatti@gmail.com>
2025-01-07 15:24:14 -0300

    Update no ConnectionFactory
    Alteração no nome do Banco de Dados

--- ControleEstoque/src/connection/ConnectionFactory.java
+++ ControleEstoque/src/connection/ConnectionFactory.java
@@ -19,7 +19,7 @@ import javax.swing.JOptionPane;
 public class ConnectionFactory {
     
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String URL = "jdbc:mysql://localhost:3306/dbGestaoEstoque";
     private static final String USER = "root";
     private static final String PASSWORD = "mysql";
     
